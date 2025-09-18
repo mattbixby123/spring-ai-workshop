fac01894a2930880e3b9af0af56dfdaa6de22c97
mattbixby123 <matthew.bixby1@gmail.com>
2025-09-18 14:33:24 -0400

    Document chunking to in-memory vector db + Models Controller

---
+++ src/main/java/dev/mattbixby/workshop/rag/Model.java
@@ -0,0 +1,4 @@
+package dev.mattbixby.workshop.rag;
+
+public record Model(String company, String model, int contextWindowSize) {
+}
---
+++ src/main/java/dev/mattbixby/workshop/rag/Models.java
@@ -0,0 +1,6 @@
+package dev.mattbixby.workshop.rag;
+
+import java.util.List;
+
+public record Models(List<Model> models) {
+}
